0b52d2eba47cd09ec80006723397c4ca2781ab5d
Huu Luan <luannguyen@hoiio.com>
2021-08-06 15:05:09 +0700

    Migrate material module

---
+++ src/app/shared/libs/material/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+const Providers = [
+  MatDividerModule,
+  MatProgressBarModule,
+  MatDialogModule,
+  ScrollingModule
+]
+
+@NgModule({
+  imports: [
+    Providers
+  ]
+})
+export class MaterialModule { }
