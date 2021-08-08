f8d4be01dc3daed832d6bb5d436aad467d7bc49f
Huu Luan <luannguyen@hoiio.com>
2021-08-08 17:10:59 +0700

    polish

--- src/app/shared/libs/material/material.module.ts
+++ src/app/shared/libs/material/material.module.ts
@@ -4,7 +4,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
 
 const Providers = [
   MatDividerModule,
@@ -12,7 +12,7 @@ const Providers = [
   MatDialogModule,
   ScrollingModule,
   MatButtonModule,
-  MatMenuModule
+  MatCardModule
 ]
 
 @NgModule({
